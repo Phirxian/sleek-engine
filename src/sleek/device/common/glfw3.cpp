1b8d1b635dd364bd59e404cf75b531f8b1cfcc34
Javayss <phirxian@gmail.com>
2024-09-11 17:46:12 +0200

    glfw key mapping

--- src/sleek/device/common/glfw3.cpp
+++ src/sleek/device/common/glfw3.cpp
@@ -64,7 +64,112 @@ namespace sleek
             bool val = false;
 
             if(action == GLFW_PRESS)
+            {
                 val = true;
+                i->type = EVENT_KEY_DOWN;
+            }
+            else if (action == GLFW_RELEASE)
+                i->type = EVENT_KEY_UP;
+            else
+                return;
+
+            /** key event **/
+            if(key == GLFW_KEY_TAB) i->setkey(KEY_TAB, val);
+            if(key == GLFW_KEY_ENTER) i->setkey(KEY_RETURN, val);
+            if(key == GLFW_KEY_RIGHT_SHIFT || key == GLFW_KEY_LEFT_SHIFT) i->setkey(KEY_SHIFT, val);
+            if(key == GLFW_KEY_RIGHT_CONTROL || key == GLFW_KEY_LEFT_CONTROL) i->setkey(KEY_CONTROL, val);
+            if(key == GLFW_KEY_MENU) i->setkey(KEY_MENU, val);
+            if(key == GLFW_KEY_PAUSE) i->setkey(KEY_PAUSE, val);
+            if(key == GLFW_KEY_ESCAPE) i->setkey(KEY_ESCAPE, val);
+            if(key == GLFW_KEY_SPACE) i->setkey(KEY_SPACE, val);
+            if(key == GLFW_KEY_END) i->setkey(KEY_END, val);
+            if(key == GLFW_KEY_HOME) i->setkey(KEY_HOME, val);
+            if(key == GLFW_KEY_LEFT) i->setkey(KEY_LEFT, val);
+            if(key == GLFW_KEY_UP) i->setkey(KEY_UP, val);
+            if(key == GLFW_KEY_RIGHT) i->setkey(KEY_RIGHT, val);
+            if(key == GLFW_KEY_DOWN) i->setkey(KEY_DOWN, val);
+            if(key == GLFW_KEY_INSERT) i->setkey(KEY_INSERT, val);
+            if(key == GLFW_KEY_DELETE) i->setkey(KEY_DELETE, val);
+            if(key == GLFW_KEY_0) i->setkey(KEY_KEY_0, val);
+            if(key == GLFW_KEY_1) i->setkey(KEY_KEY_1, val);
+            if(key == GLFW_KEY_2) i->setkey(KEY_KEY_2, val);
+            if(key == GLFW_KEY_3) i->setkey(KEY_KEY_3, val);
+            if(key == GLFW_KEY_4) i->setkey(KEY_KEY_4, val);
+            if(key == GLFW_KEY_5) i->setkey(KEY_KEY_5, val);
+            if(key == GLFW_KEY_6) i->setkey(KEY_KEY_6, val);
+            if(key == GLFW_KEY_7) i->setkey(KEY_KEY_7, val);
+            if(key == GLFW_KEY_8) i->setkey(KEY_KEY_8, val);
+            if(key == GLFW_KEY_9) i->setkey(KEY_KEY_9, val);
+            if(key == GLFW_KEY_KP_0) i->setkey(KEY_NUMPAD0, val);
+            if(key == GLFW_KEY_KP_1) i->setkey(KEY_NUMPAD1, val);
+            if(key == GLFW_KEY_KP_2) i->setkey(KEY_NUMPAD2, val);
+            if(key == GLFW_KEY_KP_3) i->setkey(KEY_NUMPAD3, val);
+            if(key == GLFW_KEY_KP_4) i->setkey(KEY_NUMPAD4, val);
+            if(key == GLFW_KEY_KP_5) i->setkey(KEY_NUMPAD5, val);
+            if(key == GLFW_KEY_KP_6) i->setkey(KEY_NUMPAD6, val);
+            if(key == GLFW_KEY_KP_7) i->setkey(KEY_NUMPAD7, val);
+            if(key == GLFW_KEY_KP_8) i->setkey(KEY_NUMPAD8, val);
+            if(key == GLFW_KEY_KP_9) i->setkey(KEY_NUMPAD9, val);
+            if(key == GLFW_KEY_A) i->setkey(KEY_KEY_A, val);
+            if(key == GLFW_KEY_B) i->setkey(KEY_KEY_B, val);
+            if(key == GLFW_KEY_C) i->setkey(KEY_KEY_C, val);
+            if(key == GLFW_KEY_D) i->setkey(KEY_KEY_D, val);
+            if(key == GLFW_KEY_E) i->setkey(KEY_KEY_E, val);
+            if(key == GLFW_KEY_F) i->setkey(KEY_KEY_F, val);
+            if(key == GLFW_KEY_G) i->setkey(KEY_KEY_G, val);
+            if(key == GLFW_KEY_H) i->setkey(KEY_KEY_H, val);
+            if(key == GLFW_KEY_I) i->setkey(KEY_KEY_I, val);
+            if(key == GLFW_KEY_J) i->setkey(KEY_KEY_J, val);
+            if(key == GLFW_KEY_K) i->setkey(KEY_KEY_K, val);
+            if(key == GLFW_KEY_L) i->setkey(KEY_KEY_L, val);
+            if(key == GLFW_KEY_M) i->setkey(KEY_KEY_M, val);
+            if(key == GLFW_KEY_N) i->setkey(KEY_KEY_N, val);
+            if(key == GLFW_KEY_O) i->setkey(KEY_KEY_O, val);
+            if(key == GLFW_KEY_P) i->setkey(KEY_KEY_P, val);
+            if(key == GLFW_KEY_Q) i->setkey(KEY_KEY_Q, val);
+            if(key == GLFW_KEY_R) i->setkey(KEY_KEY_R, val);
+            if(key == GLFW_KEY_S) i->setkey(KEY_KEY_S, val);
+            if(key == GLFW_KEY_T) i->setkey(KEY_KEY_T, val);
+            if(key == GLFW_KEY_U) i->setkey(KEY_KEY_U, val);
+            if(key == GLFW_KEY_V) i->setkey(KEY_KEY_V, val);
+            if(key == GLFW_KEY_W) i->setkey(KEY_KEY_W, val);
+            if(key == GLFW_KEY_X) i->setkey(KEY_KEY_X, val);
+            if(key == GLFW_KEY_Y) i->setkey(KEY_KEY_Y, val);
+            if(key == GLFW_KEY_Z) i->setkey(KEY_KEY_Z, val);
+            if(key == GLFW_KEY_MINUS) i->setkey(KEY_SUBTRACT, val);
+            if(key == GLFW_KEY_SLASH) i->setkey(KEY_DIVIDE, val);
+            if(key == GLFW_KEY_F1) i->setkey(KEY_F1, val);
+            if(key == GLFW_KEY_F2) i->setkey(KEY_F2, val);
+            if(key == GLFW_KEY_F3) i->setkey(KEY_F3, val);
+            if(key == GLFW_KEY_F4) i->setkey(KEY_F4, val);
+            if(key == GLFW_KEY_F5) i->setkey(KEY_F5, val);
+            if(key == GLFW_KEY_F6) i->setkey(KEY_F6, val);
+            if(key == GLFW_KEY_F7) i->setkey(KEY_F7, val);
+            if(key == GLFW_KEY_F8) i->setkey(KEY_F8, val);
+            if(key == GLFW_KEY_F9) i->setkey(KEY_F9, val);
+            if(key == GLFW_KEY_F10) i->setkey(KEY_F10, val);
+            if(key == GLFW_KEY_F11) i->setkey(KEY_F11, val);
+            if(key == GLFW_KEY_F12) i->setkey(KEY_F12, val);
+            if(key == GLFW_KEY_F13) i->setkey(KEY_F13, val);
+            if(key == GLFW_KEY_F14) i->setkey(KEY_F14, val);
+            if(key == GLFW_KEY_F15) i->setkey(KEY_F15, val);
+            if(key == GLFW_KEY_LEFT_SHIFT) i->setkey(KEY_LSHIFT, val);
+            if(key == GLFW_KEY_RIGHT_SHIFT) i->setkey(KEY_RSHIFT, val);
+            if(key == GLFW_KEY_LEFT_CONTROL) i->setkey(KEY_LCONTROL, val);
+            if(key == GLFW_KEY_RIGHT) i->setkey(KEY_RCONTROL, val);
+//            if(key == GLFW_KEY_LMETA) i->setkey(KEY_LMENU, val);
+//            if(key == GLFW_KEY_RMETA) i->setkey(KEY_RMENU, val);
+//            if(key == GLFW_KEY_PLUS) i->setkey(KEY_PLUS, val);
+            if(key == GLFW_KEY_COMMA) i->setkey(KEY_COMMA, val);
+            if(key == GLFW_KEY_MINUS) i->setkey(KEY_MINUS, val);
+            if(key == GLFW_KEY_PERIOD) i->setkey(KEY_PERIOD, val);
+//            if(key == GLFW_KEY_AT) i->setkey(KEY_ATTN, val);
+//                if(key == GLFW_KEY_c) i->setkey(KEY_CRSEL, val);
+//                if(key == GLFW_KEY_E) i->setkey(KEY_EXSEL, val);
+//                if(key == GLFW_KEY_E) i->setkey(KEY_EREOF, val);
+//                if(key == GLFW_KEY_P) i->setkey(KEY_PLAY, val);
+//                if(key == GLFW_KEY_Z) i->setkey(KEY_ZOOM, val);
+//            if(key == GLFW_KEY_PAGEUP) i->setkey(KEY_PA1, val);
 
             evt->manage(i);
             if(!evt->running())
